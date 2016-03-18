4a127fbf6d3b78a566848a5d62618a4ac2ac0c3a
seanvsville <seanvanho@gmail.com>
2016-03-17 23:16:24 -0700

    completed click

--- click/base.js
+++ click/base.js
@@ -1,5 +1,13 @@
+//Challenge: Can you create a list of all the phrases that you click?
+//Stretch: In addition to listing the phrase I clicked, can you include a timestamp?
+// Every time you click something, log it the names of the buttons
+
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-})
+});
+
+$("span").on("click", function handleClick(event) {
+ $("ul").append("<li> "+ $(this).text() + " at " + event.timeStamp +"</li>");
+});
